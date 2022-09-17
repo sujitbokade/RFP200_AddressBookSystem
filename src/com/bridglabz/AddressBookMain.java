d9de300e9f65adb0a919f863e9b3969bcaaf81b8
sujit <sujitbokade2014@gmail.com>
2022-09-17 13:20:39 +0530

    [Sujit]Edit Contact

--- src/com/bridglabz/AddressBookMain.java
+++ src/com/bridglabz/AddressBookMain.java
@@ -7,10 +7,29 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook System!!!");
-        AddressBookMain addressBookMain = new AddressBookMain();
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
 
-        addressBookMain.addContact();
+        while (true) {
+            AddressBookMain addressBookMain = new AddressBookMain();
+            System.out.println("Enter 1 For Add Contact");
+            System.out.println("Enter 2 For Edit Contact");
+            System.out.println("Enter 3 For Exit");
+            int input = sc.nextInt();
 
+            switch (input) {
+                case 1:
+                    addressBookMain.addContact();
+                case 2:
+                    addressBookMain.editContact();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     public void addContact() {
@@ -33,6 +52,36 @@ public class AddressBookMain {
         person.setEmail(sc.next());
 
         System.out.println("Add Contact Successfully");
-        System.out.println(person);
+
+    }
+
+    public void editContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name for Edit");
+        String firstName = sc.next();
+        if (firstName.equals(person.getFirstName())) {
+            System.out.println("Edit Details of Person");
+            System.out.println("Enter first name");
+            person.setFirstName(sc.next());
+            System.out.println("Enter last name");
+            person.setLastName(sc.next());
+            System.out.println("Enter address");
+            person.setAddress(sc.next());
+            System.out.println("Enter city");
+            person.setCity(sc.next());
+            System.out.println("Enter state");
+            person.setState(sc.next());
+            System.out.println("Enter ZipCode");
+            person.setZip(sc.nextInt());
+            System.out.println("Enter phoneNumber");
+            person.setPhoneNumber(sc.nextLong());
+            System.out.println("Enter Email");
+            person.setEmail(sc.next());
+
+            System.out.println(person);
+
+        } else {
+            System.out.println("Contact Not Found");
+        }
     }
 }
